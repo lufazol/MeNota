2423075e72602db2fd056be1c53ade81f43c159a
lufazol <luanfazolin@gmail.com>
2023-05-22 11:14:18 -0300

    Add files for Xcode project

---
+++ MeNota/MeNota/MeNotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeNotaApp.swift
+//  MeNota
+//
+//  Created by Luan Fazolin on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MeNotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
