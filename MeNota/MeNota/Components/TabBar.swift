c30c97ca6f7c4241257f7858f3efacd6561d481d
lufazol <luanfazolin@gmail.com>
2023-05-24 08:49:51 -0300

    Organize folders and update .gitignore once more

---
+++ MeNota/MeNota/Components/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  MeNota
+//
+//  Created by Luan Fazolin on 24/05/23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
