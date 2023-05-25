ca562598987939656fc3e53418fe1de47467af4f
lufazol <luanfazolin@gmail.com>
2023-05-25 08:29:42 -0300

    Create SearchTopToolBar

---
+++ MeNota/MeNota/Components/SearchTopToolBar.swift
@@ -0,0 +1,32 @@
+//
+//  SearchTopToolBar.swift
+//  MeNota
+//
+//  Created by Luan Fazolin on 25/05/23.
+//
+
+import SwiftUI
+
+struct SearchTopToolBar: View {
+    @State var textFieldText = ""
+    @State var dataArray: [String] = []
+
+    var body: some View {
+        HStack {
+            SearchBar()
+                .padding(.leading)
+            
+            Button(action: {
+            }) {
+                Text("Cancel")
+            }
+            .padding()
+        }
+    }
+}
+
+struct SearchTopToolBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTopToolBar()
+    }
+}
